a921eb8098a46ba8014600a04d33601fc4b34ded
mzelazko <mzelazko50@gmail.com>
2024-11-23 15:51:00 +0100

    add more operands to real tests

--- lab06/tests_real.py
+++ lab06/tests_real.py
@@ -36,22 +36,80 @@ def whitley(x, cost=0):
             result += (y ** 2)/4000 - np.cos(y) + 1
     return result, cost + 1
 
+# TODO Does this work?
+@njit
+def weighted_random_choice(pop_size, selection_probabilities):
+    cumulative_sum = np.cumsum(selection_probabilities)
+    r = np.random.rand()
+    for i in range(pop_size):
+        if r <= cumulative_sum[i]:
+            return i
+
+
 # WE HAVE PERMISSION ONLY FROM: lectures.
 # TODO rank selection
+# @njit
+# def select(population, scores, selection_type="tournament", k=2, n_elites=2):
+#     if selection_type == "tournament":
+#         n_selected = len(population)-n_elites
+#         selected = np.zeros((n_selected,) + population[0].shape, dtype=population.dtype)
+#         selected_scores = np.zeros(n_selected, dtype=scores.dtype)
+#         for i in range(n_selected):
+#             indices = np.random.choice(len(population), k, replace=False)
+#             best = np.min(scores[indices])
+#             best_index = np.where(scores == best)[0][0]
+#             selected[i] = population[best_index]
+#             selected_scores[i] = best
+#         return selected, selected_scores
 @njit
-def select(population, scores, selection_type="tournament", k=2, n_elites=2):
+def select(population, scores, selection_type="tournament", k=2, a=0.0005, epsilon=1e-6, n_elites=2):
+    pop_size = len(population)
+    n_selected = pop_size-n_elites
+    selected = np.zeros((n_selected,) + population[0].shape, dtype=population.dtype)
+    selected_scores = np.zeros(n_selected, dtype=scores.dtype)
+    
     if selection_type == "tournament":
-        n_selected = len(population)-n_elites
-        selected = np.zeros((n_selected,) + population[0].shape, dtype=population.dtype)
-        selected_scores = np.zeros(n_selected, dtype=scores.dtype)
         for i in range(n_selected):
-            indices = np.random.choice(len(population), k, replace=False)
+            indices = np.random.choice(pop_size, k, replace=False)
             best = np.min(scores[indices])
             best_index = np.where(scores == best)[0][0]
             selected[i] = population[best_index]
             selected_scores[i] = best
-        return selected, selected_scores
 
+
+            # DIFFERENT OPTION
+            # indices = np.random.choice(pop_size, k, replace=False)
+            # best_index = indices[np.argmin(scores[indices])]  # Use indices directly with np.argmin
+            # selected[i] = population[best_index]
+            # selected_scores[i] = scores[best_index]
+
+    if selection_type == "rank":
+        sorted_indices = np.argsort(scores)[::-1]  # Sort in descending order
+        #a = 1 / (pop_size*10)
+        d = ((2 * 1) / pop_size - (2 * a)) / (pop_size - 1)
+        selection_probabilities = np.zeros(pop_size)  # Initialize selection probabilities
+        for i in range(pop_size):
+            cur_index = sorted_indices[i]
+            selection_probabilities[cur_index] = a + i*d
+        if not np.isclose(np.sum(selection_probabilities), 1.0, atol=1e-9):
+            print("ERROR: Selection probabilities do not sum to 1.")
+        # Select individuals based on ranking probabilities
+        for i in range(n_selected):
+            #chosen_index = np.random.choice(pop_size, p=selection_probabilities)  # Rank-based selection
+            chosen_index = weighted_random_choice(pop_size, selection_probabilities)
+            selected[i] = population[int(chosen_index)]
+            selected_scores[i] = scores[int(chosen_index)]
+    if selection_type == "roulette":
+        fitness = 1 / (epsilon + scores)
+        selection_probabilities = fitness / np.sum(fitness)
+
+        for i in range(n_selected):
+            #chosen_index = np.random.choice(pop_size, p=selection_probabilities)
+            chosen_index = weighted_random_choice(pop_size, selection_probabilities)
+            selected[i] = population[int(chosen_index)]
+            selected_scores[i] = scores[int(chosen_index)]
+
+    return selected, selected_scores
 
 # Function to decrease the crossover probability based on evaluations
 #@njit
@@ -72,36 +130,49 @@ def reflective_clipping(value, lower, upper):
 # It only works when parents are even.
 # WE HAVE PERMISSION ONLY FROM: lectures + literature
 @njit
-def crossover(parents, scores, crossover_type="weighted", f=1):
+def crossover(parents, scores, crossover_type, eta, f):
 
     #crossover_prob = get_crossover_probability()
 
     parents_len = len(parents)
-
     offspring = parents.copy()  
 
-    parents = parents[np.argsort(scores)[::-1]]
-    weights = np.array([np.log(parents_len + 1) - np.log(i+1) for i in range(parents_len)]) # TODO fix contradiction to formula. miu doesn't equal POP_SIZE - ELITE_SIZE, but POP_SIZE-ELITE_SIZE-1
+    if crossover_type == "weighted":
+        parents = parents[np.argsort(scores)[::-1]]
+        weights = np.array([np.log(parents_len + 1) - np.log(i+1) for i in range(parents_len)])
+        
+        sigma_term = np.zeros(parents.shape[2])  # One entry per dimension (sigma)
+        xi_term = np.zeros(parents.shape[2])     # One entry per dimension (xi)
+
+        # Calculate weighted sum for sigma_term and xi_term across all parents for each dimension
+        for j in range(parents.shape[2]):  # Loop over each dimension
+            sigma_term[j] = np.sum(parents[:, 1, j] * weights) / np.sum(weights)  # Weighted sum for sigma
+            xi_term[j] = np.sum(parents[:, 0, j] * weights) / np.sum(weights)  # Weighted sum for xi
+
+
+        for i in range(parents_len):
+            n = len(parents[i][0])
+            random_for_all = np.random.normal(0, 1)
+            for j in range(n):
+                sigma = sigma_term[j] * np.exp(
+                random_for_all / np.sqrt(2 * n) + np.random.normal(0, 1) / np.sqrt(2 * np.sqrt(n))
+                )
+                offspring[i][1][j] = reflective_clipping(sigma, 0, 1)
+                xi = xi_term[j] + offspring[i][1][j] * np.random.normal(0, 1)
+                offspring[i][0][j] = reflective_clipping(xi, DOMAINS[f][0], DOMAINS[f][1])
+
+    elif crossover_type == "SBX":
+        u = np.random.uniform(0, 1)
+        if u <= 0.5:
+            beta = (2*u)**(1/(1+eta))
+        else:
+            beta = (1/(2*(1-u)))**(1/(1+eta))
+        for i in range(0, parents_len, 2):
+            n = len(parents[i][0])
+            for j in range(n):
+                offspring[i][0][j] = reflective_clipping(0.5 * ((1 + beta) * parents[i][0][j] + (1 - beta) * parents[i + 1][0][j]), DOMAINS[f][0], DOMAINS[f][1]) 
+                offspring[i + 1][0][j] = reflective_clipping(0.5 * ((1 - beta) * parents[i][0][j] + (1 + beta) * parents[i + 1][0][j]), DOMAINS[f][0], DOMAINS[f][1]) 
     
-    sigma_term = np.zeros(parents.shape[2])  # One entry per dimension (sigma)
-    xi_term = np.zeros(parents.shape[2])     # One entry per dimension (xi)
-
-    # Calculate weighted sum for sigma_term and xi_term across all parents for each dimension
-    for j in range(parents.shape[2]):  # Loop over each dimension
-        sigma_term[j] = np.sum(parents[:, 1, j] * weights) / np.sum(weights)  # Weighted sum for sigma
-        xi_term[j] = np.sum(parents[:, 0, j] * weights) / np.sum(weights)  # Weighted sum for xi
-
-
-    for i in range(parents_len):
-        n = len(parents[i][0])
-        random_for_all = np.random.normal(0, 1)
-        for j in range(n):
-            sigma = sigma_term[j] * np.exp(
-            random_for_all / np.sqrt(2 * n) + np.random.normal(0, 1) / np.sqrt(2 * np.sqrt(n))
-            )
-            offspring[i][1][j] = reflective_clipping(sigma, 0, 1)
-            xi = xi_term[j] + offspring[i][1][j] * np.random.normal(0, 1)
-            offspring[i][0][j] = reflective_clipping(xi, DOMAINS[f][0], DOMAINS[f][1])
 
     return offspring
 
@@ -131,10 +202,10 @@ def evaluate(f, population, cost):
 
     scores = np.zeros(len(population))
     for i in range(len(population)):
-        xi = population[i][0] # TODO debug if works
-        if f == 1:
+        xi = population[i][0]
+        if f == 0:
             scores[i], cost = rosenbrock(xi, cost)
-        elif f == 2:
+        elif f == 1:
             scores[i], cost = salomon(xi, cost)
         else:
             scores[i], cost = whitley(xi, cost)
@@ -158,15 +229,27 @@ def objective(trial):
     pop_size = trial.suggest_categorical("pop_size", [20, 22, 24, 26, 28, 30]) # Has to be even
     n_elites = trial.suggest_categorical("n_elites", [0, 2, 4, 6]) # Has to be even
     #GRAY = trial.suggest_categorical("GRAY", [True, False])
-    #selection_type = trial.suggest_categorical("selection_type", ["rank", "tournament"])
-    selection_type = "tournament"
-    #crossover_type = trial.suggest_categorical("crossover_type", ["one_point", "two_point", "uniform"])
+    selection_type = trial.suggest_categorical("selection_type", ["tournament", "rank", "roulette"])
+    #selection_type = "rank"
+    crossover_type = trial.suggest_categorical("crossover_type", ["weighted", "SBX"])
+    #crossover_type = "SBX"
+    if crossover_type == "SBX":
+        eta = trial.suggest_int("eta", 10, 20)
+    else:
+        eta = 1
 
     if selection_type == "tournament":
         k = trial.suggest_int("k", 2, 6)
-    else:
-        k = None
-
+        a = 1
+        epsilon = 1
+    elif selection_type == "rank":
+        k = 1
+        a = trial.suggest_float("a", 0.00005, 0.005)
+        epsilon = 1
+    elif selection_type == "roulette":
+        k = 1
+        a = 1
+        epsilon = trial.suggest_float("epsilon", 1e-9, 1)
 
     n=15 # Number of dimensions
     max_cost = 10000*n  # Max evaluations
@@ -184,14 +267,11 @@ def objective(trial):
 
             cost = 0
             #CUR_EVALUATIONS = cost
-            scores, cost = evaluate(f=f, population=population, cost=cost)
+            scores, cost = evaluate(f, population, cost)
             best_x, best_score = population[i := np.argmin(scores)], scores[i]
             while cost < max_cost: # TODO loop should end when max_cost is reached, so better to do it in a evaluation function
-                if selection_type == "tournament":
-                    selected, selected_scores = select(population, scores, selection_type, k, n_elites)
-                elif selection_type == "rank":
-                    selected, selected_scores = select(population, scores, selection_type, n_elites)
-                offspring = crossover(selected, selected_scores, "weighted", f)
+                selected, selected_scores = select(population, scores, selection_type, k, a, epsilon, n_elites)
+                offspring = crossover(selected, selected_scores, crossover_type, eta, f)
                 offspring = mutate(offspring, f)
                 population = add_elites(population, offspring, scores, n_elites)
                 scores, cost = evaluate(f=f, population=population, cost=cost)
@@ -215,10 +295,13 @@ def objective(trial):
 
 
 def main():
+    print(objective.__code__)
+    print(evaluate.__code__)
+
     database_url = "sqlite:///study.db?timeout=60"
-    study = optuna.create_study(direction="minimize", storage=database_url, study_name="real_test_1", load_if_exists=True)
-    #study = optuna.create_study(direction="minimize")  # Minimize the cost or maximize the fitness
-    study.optimize(objective, n_trials=10, n_jobs=5) # 2 used cores
+    #study = optuna.create_study(direction="minimize", storage=database_url, study_name="real_test_1", load_if_exists=True)
+    study = optuna.create_study(direction="minimize")  # Minimize the cost or maximize the fitness
+    study.optimize(objective, n_trials=10, n_jobs=1) # 2 used cores
 
 # Retrieve and display trial results
     print("Best trial:")
